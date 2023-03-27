e1f9960ef1b4c810a772d09ab1ec22d60334ab39
Felip <Felip@DESKTOP-C1CRMRR>
2023-03-27 12:29:57 -0500

    Add project files.

---
+++ SpaceInvaders/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace SpaceInvaders
+{
+    partial class Invaders
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblscore = new System.Windows.Forms.Label();
+            this.TmrGame = new System.Windows.Forms.Timer(this.components);
+            this.picrick = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picrick)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblscore
+            // 
+            this.lblscore.AutoSize = true;
+            this.lblscore.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblscore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblscore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblscore.Location = new System.Drawing.Point(12, 533);
+            this.lblscore.Name = "lblscore";
+            this.lblscore.Size = new System.Drawing.Size(103, 16);
+            this.lblscore.TabIndex = 0;
+            this.lblscore.Text = "Puntuación:  0";
+            // 
+            // TmrGame
+            // 
+            this.TmrGame.Interval = 20;
+            this.TmrGame.Tick += new System.EventHandler(this.GameTimer);
+            // 
+            // picrick
+            // 
+            this.picrick.Image = global::SpaceInvaders.Properties.Resources.picrick;
+            this.picrick.Location = new System.Drawing.Point(333, 499);
+            this.picrick.Name = "picrick";
+            this.picrick.Size = new System.Drawing.Size(100, 50);
+            this.picrick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picrick.TabIndex = 1;
+            this.picrick.TabStop = false;
+            this.picrick.Click += new System.EventHandler(this.picrick_Click);
+            // 
+            // Invaders
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(734, 561);
+            this.Controls.Add(this.picrick);
+            this.Controls.Add(this.lblscore);
+            this.Name = "Invaders";
+            this.Text = "SpaceInvaders";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picrick)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblscore;
+        private System.Windows.Forms.PictureBox picrick;
+        private System.Windows.Forms.Timer TmrGame;
+    }
+}
+
---
+++ SpaceInvaders/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.DirectoryServices.ActiveDirectory;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace SpaceInvaders
+{
+    public partial class Invaders : Form
+    {
+        //To move the ship from left to right
+        bool DirLeft, DirRight;
+        int RickSpeed = 12;
+        int CromulonSpeed = 5;
+        int GameScore = 0;
+        int CromulonLaserTmr = 300;
+
+        PictureBox[] CromulonsArray;
+
+        bool lasershoot;
+        bool GameFinished;
+
+        public Invaders()
+        {
+            InitializeComponent();
+            GameCode();
+        }
+        private void GameTimer(object sender, EventArgs e)
+        {
+            lblscore.Text = "Puntaje: " + GameScore;
+            if (DirLeft)
+            {
+                picrick.Left -= RickSpeed;
+            }
+            if (DirRight)
+            {
+                picrick.Left += RickSpeed;
+            }
+
+            CromulonLaserTmr -= 10;
+
+            if (CromulonLaserTmr < 1)
+            {
+                CromulonLaserTmr = 300;
+                LaserMaker("LaserCromulon");
+            }
+            foreach (Control x in this.Controls)
+            {
+                //this defines the positions of the enemies
+                if (x is PictureBox && (string)x.Tag == "Cromulons")
+                {
+                    x.Left += CromulonSpeed;
+
+                    if (x.Left > 730)
+                    {
+                        x.Top += 65;
+                        x.Left = -80;
+                    }
+
+                    if (x.Bounds.IntersectsWith(picrick.Bounds))
+                    {
+                        GameOver("Perdiste!");
+                    }
+
+                    foreach (Control y in this.Controls)
+                    {
+                        if (y is PictureBox && (string)y.Tag == "laserrick")
+                        {
+                            if (y.Bounds.IntersectsWith(x.Bounds))
+                            {
+                                this.Controls.Remove(x);
+                                this.Controls.Remove(y);
+                                GameScore += 1;
+                                lasershoot = false;
+                            }
+                        }
+                    }
+                }
+
+                if (x is PictureBox && (string)x.Tag == "laserrick")
+                {
+                    x.Top -= 20;
+
+                    if (x.Top < 15)
+                    {
+                        this.Controls.Remove(x);
+                        lasershoot = false;
+                    }
+                }
+                //this defines the position of the enemy laser
+                if (x is PictureBox && (string)x.Tag == "LaserCromulon")
+                {
+
+                    x.Top += 20;
+
+                    if (x.Top > 620)
+                    {
+                        this.Controls.Remove(x);
+                    }
+
+                    if (x.Bounds.IntersectsWith(picrick.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        
+                    }
+
+                }
+            }
+
+            if (GameScore > 8)
+            {
+                CromulonSpeed = 12;
+            }
+
+            if (GameScore == CromulonsArray.Length)
+            {
+                GameOver("Ganaste!");
+            }
+        }
+
+        private void KDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                DirLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                DirRight = true;
+            }
+        }
+        //both for the movement of the ship, from left to right with the keys
+        private void KUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                DirLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                DirRight = false;
+            }
+            if (e.KeyCode == Keys.Space && lasershoot == false)
+            {
+                lasershoot = true;
+                LaserMaker("laserrick");
+            }
+            if (e.KeyCode == Keys.Enter && GameFinished == true)
+            {
+                Clear();
+                GameCode();
+            }
+        }
+        private void MakeCromulons()
+        {
+            CromulonsArray = new PictureBox[15];
+
+            int left = 0;
+            //this makes the cromulons, with the array previosly defined, can be modified easily
+            for (int i = 0; i < CromulonsArray.Length; i++)
+            {
+                CromulonsArray[i] = new PictureBox();
+                CromulonsArray[i].Size = new Size(40, 50);
+                CromulonsArray[i].Image = Properties.Resources.Cromulon;
+                CromulonsArray[i].Top = 5;
+                CromulonsArray[i].Tag = "Cromulons";
+                CromulonsArray[i].Left = left;
+                CromulonsArray[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                this.Controls.Add(CromulonsArray[i]);
+                left = left - 80;
+            }
+        }
+
+        private void GameCode()
+        {
+            //this stores the speed, score and some of the ticks
+            lblscore.Text = "Puntuación: 0";
+            GameScore = 0;
+            GameFinished = false;
+
+            CromulonLaserTmr = 300;
+            CromulonSpeed = 5;  
+            lasershoot = false;
+
+            MakeCromulons();
+            TmrGame.Start();
+        }
+
+        private void GameOver(string msg)
+        {
+            //this defines the score at the end of the game
+            GameFinished = true;
+            TmrGame.Stop();
+            lblscore.Text = "Score: " + GameScore + " " + msg;
+        }
+
+        private void Clear()
+        {
+            foreach (PictureBox i in CromulonsArray)
+            {
+                this.Controls.Remove(i);
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox)
+                {
+                    if ((string)x.Tag == "laserrick" || (string)x.Tag == "LaserCromulon")
+                    {
+                        this.Controls.Remove(x);
+                    }
+                }
+            }
+        }
+
+        private void picrick_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LaserMaker(string LaserTag)    
+        {
+            //this creates the laser picturebox, and defines it's starting point 
+            PictureBox laser = new PictureBox();
+            laser.Image = Properties.Resources.laserr;
+            laser.Size = new Size(10, 20);
+            laser.Tag = LaserTag ;
+            laser.Left = picrick.Left + picrick.Width / 2;
+            if ((string)laser.Tag == "laserrick")
+            {
+                laser.Top = picrick.Top - 20;
+            }
+            else if ((string)laser.Tag == "LaserCromulon")
+            {
+                laser.Top = -100;
+            }
+            this.Controls.Add(laser);
+            laser.BringToFront();
+        }
+                    
+    }
+
+}
